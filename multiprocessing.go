1f9b161b2eeb1278cde7fa02e89baabc74fd3858
MIAUSEproject-founderKJ <155000923+MIAUSEproject-founderKJ@users.noreply.github.com>
2025-10-13 16:30:17 +0800

    Update multiprocessing.go

--- multiprocessing.go
+++ multiprocessing.go
@@ -2,59 +2,42 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"sync"
+	"os"
+	"os/exec"
 	"time"
 )
 
-// Function simulating microphone input
-func listenMicrophone(q chan string, wg *sync.WaitGroup) {
-	defer wg.Done()
-	for {
-		time.Sleep(time.Duration(rand.Intn(1000)+500) * time.Millisecond)
-		commands := []string{"turn on the light", "move forward", "stop"}
-		text := commands[rand.Intn(len(commands))]
-		fmt.Println("[Mic] Heard:", text)
-		q <- text
-	}
-}
-
-// Function processing commands
-func processCommand(q chan string, wg *sync.WaitGroup) {
-	defer wg.Done()
-	for text := range q {
-		fmt.Println("[Processor] Executing command for:", text)
-		time.Sleep(500 * time.Millisecond)
-	}
-}
-
-// Background monitor
-func backgroundMonitor(wg *sync.WaitGroup) {
-	defer wg.Done()
-	for {
-		fmt.Println("[Monitor] System OK")
-		time.Sleep(3 * time.Second)
-	}
-}
-
 func main() {
-	rand.Seed(time.Now().UnixNano())
-	q := make(chan string, 10) // buffered channel
-	var wg sync.WaitGroup
+	// List of commands to run as separate processes
+	commands := []string{"process1", "process2", "process3"}
 
-	wg.Add(3)
-	go listenMicrophone(q, &wg)
-	go processCommand(q, &wg)
-	go backgroundMonitor(&wg)
+	var procs []*exec.Cmd
 
-	// Let goroutines run for 10 seconds
+	for _, cmdName := range commands {
+		// Here we assume process1, process2, process3 are executables in your PATH
+		cmd := exec.Command(cmdName)
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+
+		err := cmd.Start()
+		if err != nil {
+			fmt.Println("Error starting", cmdName, ":", err)
+			continue
+		}
+
+		procs = append(procs, cmd)
+		fmt.Println(cmdName, "started with PID", cmd.Process.Pid)
+	}
+
+	// Let them run for 10 seconds
 	time.Sleep(10 * time.Second)
 
 	fmt.Println("Terminating processes...")
 
-	// Graceful shutdown
-	close(q)
-
-	// Optional: Wait for goroutines that finish when q is closed
-	time.Sleep(1 * time.Second)
+	// Kill all processes
+	for _, cmd := range procs {
+		if cmd.Process != nil {
+			cmd.Process.Kill()
+		}
+	}
 }

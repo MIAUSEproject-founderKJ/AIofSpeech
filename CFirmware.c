463bca76d80aca59abc08b3933badcd870a77b4f
MIAUSEproject-founderKJ <155000923+MIAUSEproject-founderKJ@users.noreply.github.com>
2025-10-14 09:57:42 +0800

    Create FirmwareExample.c

---
+++ CFirmware.c
@@ -0,0 +1,15 @@
+// firmware.c
+//Waits for strings like "SET_SPEED 100" or "STOP".
+//Parses and executes them directly on hardware.
+//Responds back with text over UART.
+
+#include <stdio.h>
+#include <string.h>
+#include "uart.h"
+#include "motor.h"
+
+void processCommand(const char *cmd){
+  if (strncmp(cmd,"SET_SPEED", 9)==0) {
+    int spd = atoi(cmd+10); 
+  }
+}
